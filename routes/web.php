456fa8f1d458b2873132c38b2e33b1cb3a4b2762
ayaka <grape8ayk208@yahoo.co.jp>
2020-05-21 08:15:04 +0000

    Add web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create',
+'Admin\NewsController@add');
+});
+
+//課題3 
+Route::get('/XXX', 'AAAController@bbb');
+
+//課題4
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create',
+'ProfileController@add');
+    Route::get('profile/edit', 
+'ProfileController@edit');
+});
