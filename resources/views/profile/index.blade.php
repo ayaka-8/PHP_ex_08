822ab091817caaf97bf4855fb868ff58c9bc4bc7
EC2 Default User <ec2-user@ip-172-31-1-173.us-east-2.compute.internal>
2020-05-30 08:41:48 +0000

    PHP課題18 提出

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.front_profile')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>ユーザー一覧</h2>
+        </div>
+        <hr color="#c0c0c0">
+            <div class="row">
+                <div class="profiles col-md-8 mx-auto mt-3">
+                    @foreach($profiles as $profile)
+                        <div class="profile">
+                            <div class="row">
+                                <div class="text col-md-6">
+                                    <div class="title">
+                                        <p>氏名　{{ $profile->name }}</p>
+                                    </div>
+                                    <div class="date">
+                                        <p>更新日　{{ $profile->updated_at->format('Y年m月d日') }}</p>
+                                    </div>
+                                
+                                </div>
+                            
+                            </div>
+                        </div>
+                        <hr color="#c0c0c0">
+                    @endforeach
+                </div>
+            </div>
+    </div>
+@endsection
